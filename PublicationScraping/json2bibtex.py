9cc740cc1a3801ecefd5a95798ecf0f0ad8ccb14
Jake Bergquist <38257234+jab0707@users.noreply.github.com>
2025-01-17 13:59:16 -0700

    Academic tracker dev (#5)
    * Update update_author_pubs.py
    
    * Update update_author_pubs.py
    
    * Update update_author_pubs.py
    
    * Update update_author_pubs.py
    
    * Update update_author_pubs.py
    
    * Update update_author_pubs.py
    
    * Update update_author_pubs.py
    
    * Getting first working example of publication scraping using academic tracker
    
    * adding json2bibtex conversion skeleton
    
    * adding json2bibtex functionality
    
    ---------
    
    Co-authored-by: Jake Bergquist <jbergquist@cibcsm4.sci.utah.edu>

---
+++ PublicationScraping/json2bibtex.py
@@ -0,0 +1,129 @@
+import json as js
+from pathlib import Path
+import os,fire
+#Helper to convert json from academic tracker into a bibtex file
+#Author: Jake Bergquist
+
+def logInfo(message,logFile=None):
+	'''
+	I know I know I know that logging exists, but it is so finnicky
+	and I got frustrated
+	So here we are with my own simple logger
+	'''
+	print(message)
+	if logFile is not None:
+		with open(logFile, "a") as log:
+			log.write(message)
+
+
+def stringCleaner(inStr):
+	#cleans up common artifacts from the academic tracker
+	removeList = ['...']
+	for rm in removeList:
+		inStr=inStr.replace(rm,'')
+	replaceDict={}
+	for k in replaceDict.keys():
+		inStr=inStr.replace(k,replaceDict[k])
+	return inStr
+
+def jsonAuthorToBibtex(authors):
+	authorString = ''
+	for author in authors:
+		authorString+=f'{author['initials']} {author['lastname']} and '
+	authorString = authorString[:-4]
+	authorString = bibFieldFormat(authorString,'author')
+	return authorString
+
+def bibFieldFormat(inStr,name,clean=False):
+	if clean:
+		inStr = stringCleaner(inStr)
+	inStr = bibBracketWrap(inStr)
+	if len(name)<4:
+		name = name + '\t'
+	inStr = name + '\t=\t' + inStr
+	return inStr
+
+def bibBracketWrap(inStr):
+	if inStr is None:
+		inStr = ''
+	if type(inStr) is list:
+		inStr = ', '.join(inStr)
+	return "{" + inStr + "}"
+
+class pub:
+	def __init__(self,key,title,authors,journal,year,pubType,otherFields):
+		self.pubKey=key
+		self.authors=authors
+		self.title=title
+		self.journal=journal
+		self.year=year
+		self.pubType=pubType
+		self.fields = ['title','authors','year','journal']
+		[print(f'Adding {field}') for field in self.fields]
+		for key in otherFields.keys():
+			print(f'adding {key}')
+			setattr(self,key,otherFields[key])
+			self.fields.extend([key])
+		print(f'Publication set up with these fields: {self.fields}')
+
+	
+	@classmethod
+	def from_json_dict(cls,pubDict):
+		firstAuthor = pubDict['authors'][0]
+		authorKey = firstAuthor['lastname'][0:3]
+		year = f"{pubDict['publication_date']['year']}"
+		date = f"{year}-{pubDict['publication_date']['month']}"
+
+		pubKey = authorKey+year
+		title 	= bibFieldFormat(pubDict['title'], 'title')
+		authors = jsonAuthorToBibtex(pubDict['authors'])
+		journal = bibFieldFormat(pubDict['journal'],'journal',True)
+		year 	= bibFieldFormat(year, 'year')
+		#other fields:
+		doi 	= bibFieldFormat(pubDict['doi'], 'doi')
+		date 	= bibFieldFormat(date, 'date')
+		pmid 	= bibFieldFormat(pubDict['pubmed_id'], 'pmid')
+		grants 	= bibFieldFormat(pubDict['grants'], 'grants')
+
+		otherFields = dict(zip(['doi','date','pmid','grants'],[doi,date,pmid,grants]))
+
+		return cls(pubKey,title,authors,journal,year,'article',otherFields)
+
+
+
+
+
+
+
+	def __str__(self):
+		baseStr = "@" + self.pubType + "{" + self.pubKey + ",\n"
+		for field in self.fields:
+			baseStr += f'\t{getattr(self,field)},\n'
+		baseStr=baseStr[:-2]#remove last comma
+		baseStr+='\n}\n'
+		return baseStr
+	def __repr__(self):
+		return self.__str__()
+
+
+
+def convertJson(inFile,outFile='out.bib',keyPrefix="SCI:"):
+	print(f'Loading {inFile}')
+	with open(inFile) as f:
+		inputData = js.load(f)
+	Path(outFile).touch()
+	with open(outFile,'w') as f:
+		f.write('')
+	for pubId in inputData.keys():
+		print(f"working on {pubId}")
+		pubDict = inputData[pubId]
+		currentPublication = pub.from_json_dict(pubDict)
+		currentPublication.pubKey =keyPrefix + currentPublication.pubKey
+		with open(outFile,'a') as f:
+			f.write(str(currentPublication))
+		
+
+
+
+if __name__=="__main__":
+	fire.Fire({'convert':convertJson})
--- PublicationScraping/update_author_pubs.py
+++ PublicationScraping/update_author_pubs.py
@@ -1,44 +1,57 @@
-import fire, sys, os, glob, subprocess, logging
+import fire
+import sys, os, glob, subprocess, logging
 from datetime import datetime
 
-DEFAULT_CONFIG_LOCATION="./authorConfigFiles/"
-DEFAULT_ACADEMIC_TRACKER_LOCATION="academic_tracker"
-DEFAULT_ARGUMENTS="--test --no-ORCID --verbose"
-DEFAULT_OUTFILE_LOCATION="./logFiles/"
+SCRIPT_DIR=os.path.dirname(os.path.realpath(__file__))
 
-def updatePubs(author="all",arguments=DEFAULT_ARGUMENTS,script_loc=DEFAULT_ACADEMIC_TRACKER_LOCATION,config_loc=DEFAULT_CONFIG_LOCATION):
+DEFAULT_CONFIG_LOCATION=SCRIPT_DIR+"/authorConfigFiles/"
+DEFAULT_ACADEMIC_TRACKER_LOCATION="academic_tracker"
+DEFAULT_ARGUMENTS=["--test", "--no-ORCID", "--verbose"]
+DEFAULT_OUTFILE_LOCATION="./logFiles/"
+RECURSION_LIMIT=1
+
+
+def logInfo(message,logFile=None):
+	'''
+	I know I know I know that logging exists, but it is so finnicky
+	and I got frustrated
+	So here we are with my own simple logger
+	'''
+	print(message)
+	if logFile is not None:
+		with open(logFile, "a") as log:
+			log.write(message)
+
+
+def updatePubs(author="all",arguments=DEFAULT_ARGUMENTS,script_loc=DEFAULT_ACADEMIC_TRACKER_LOCATION,config_loc=DEFAULT_CONFIG_LOCATION,depth=0):
+	logfile_name=f'{DEFAULT_OUTFILE_LOCATION}{author}-{date}.log'
+	if not os.path.isdir(DEFAULT_OUTFILE_LOCATION):
+		os.mkdir(DEFAULT_OUTFILE_LOCATION)
+
+	if depth > RECURSION_LIMIT:
+		logInfo("RECURSION LIMIT HIT. SOMETHING IS WRONG WITH AUTHOR LIST.",logfile_name)
+		return
 	if author == "all":
 		configFiles = glob.glob(f'{config_loc}*.json')
 		for file in configFiles:
 			name = os.path.basename(file).replace('.json','')
-			updatePubs(author=name)
+			updatePubs(name,arguments,script_loc,config_loc,depth+1)
 		return
-
-	print(f'Running pub search for {author}')
 
 	# Configure logging
 	date = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
 
-	logging.basicConfig(
-	    filename=f'{DEFAULT_OUTFILE_LOCATION}{author}-{date}.log',  # Log file name
-	    level=logging.INFO,     # Log level
-	    format='%(asctime)s - %(levelname)s - %(message)s',  # Log format
-	    datefmt='%Y-%m-%d %H:%M:%S',  # Date format
-	    handlers=[
-        logging.FileHandler(log_filename),  # Log to file
-        logging.StreamHandler()            # Log to terminal
-    ]
-	)
-
-	arguments = [f"{config_loc}{author}.json"] + arguments
-	runCommand = [script_loc] + arguments
-	logging.info(f'Run command {runCommand}')
-	result = subprocess.run(runCommand, text=True, capture_output=True)
+	logInfo(f'Running pub search for {author}',logfile_name)
 
 	
-	logging.info("Standard Output:", result.stdout)
-	logging.info("Standard Error:", result.stderr)
 
+	arguments = ["author_search", f"{config_loc}{author}.json"] + arguments
+	runCommand = [script_loc] + arguments
+	logInfo(f'Run command: {runCommand}',logfile_name)
+	result = subprocess.run(runCommand, text=True, capture_output=True)
+
+	logInfo(f'Standard output: {result.stdout}',logfile_name)
+	logInfo(f'Standard Error: {result.stderr}',logfile_name)
 
 
 
@@ -46,4 +59,4 @@ def updatePubs(author="all",arguments=DEFAULT_ARGUMENTS,script_loc=DEFAULT_ACADE
 
 
 if __name__=="__main__":
-	fire.fire({'update':updatePubs})
+	fire.Fire({'update':updatePubs})
